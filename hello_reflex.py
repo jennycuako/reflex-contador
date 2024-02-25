4a8537a698953bd9cdf4ac1f347da904e581dc1b
jennycuako <124405075+jennycuako@users.noreply.github.com>
2024-02-24 22:41:57 -0300

    Add files via upload

---
+++ hello_reflex.py
@@ -0,0 +1,38 @@
+import reflex as rx
+
+class State(rx.State):
+    
+    count: int = 0
+    
+    def increment(self):
+        self.count += 1
+        
+    def decrement(self):
+        self.count -= 1   
+
+
+def index():
+    return rx.vstack(
+        
+    rx.hstack(
+        rx.button(
+            "Decrement",
+            color_scheme="red",
+            border_radius="1em",
+            on_click=State.decrement
+           
+        ),
+        rx.heading(State.count, font_size="2em"),
+        rx.button(
+            "Increment",
+            color_scheme="green",
+            border_radius="1em",  
+            on_click=State.increment
+        ),
+    )
+)
+    
+    
+app = rx.App()
+app.add_page(index)
+app.compile()
